9c046512946c66cbba4e268d3a71b76bcb0975cc
amnahamid111 <shareknow29@gmail.com>
2026-07-30 12:39:10 -0700

    Update app.py

--- app.py
+++ app.py
@@ -292,35 +292,33 @@ if uploaded_file:
     elif final_score >= 50:
         st.warning("⚠ Potential Candidate")
 
-st.markdown("---")
+   st.markdown("---")
 
-col1, col2 = st.columns(2)
+   col1, col2 = st.columns(2)
+    with col1:
 
-with col1:
+       st.subheader("📋 Candidate Summary")
 
-    st.subheader("📋 Candidate Summary")
+       st.write(f"Experience: {experience} Years")
+       st.write(f"Education: {education}")
+       st.write(f"Skills Found: {len(skills)}")
+       st.write(f"Matched Skills: {matched}/{len(required_skills)}")
+    with col2:
 
-    st.write(f"Experience: {experience} Years")
-    st.write(f"Education: {education}")
-    st.write(f"Skills Found: {len(skills)}")
-    st.write(f"Matched Skills: {matched}/{len(required_skills)}")
+       st.subheader("🎯 Recruitment Decision")
 
-with col2:
+       if final_score >= 75:
+           st.success("✅ Recommended for Interview")
 
-    st.subheader("🎯 Recruitment Decision")
+       elif final_score >= 50:
+           st.warning("⚠ Potential Candidate")
 
-    if final_score >= 75:
-        st.success("✅ Recommended for Interview")
+       else:
+           st.error("❌ Not Suitable For This Job")
 
-    elif final_score >= 50:
-        st.warning("⚠ Potential Candidate")
+    st.markdown("---")
 
-    else:
-        st.error("❌ Not Suitable For This Job")
-
-st.markdown("---")
-
-with st.expander("📄 View Resume Text"):
+    with st.expander("📄 View Resume Text"):
 
     st.text_area(
         "Resume Content",
